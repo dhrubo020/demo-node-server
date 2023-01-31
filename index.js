70ce69d678a8c2bef5fa73ecc3b94c42178c14bc
Diptonil Singho <diptonil.singho@brainstation-23.com>
2023-01-31 18:00:55 +0600

    bump

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+app.get("/a", function (req, res) {
+  res.send("Hello World!");
+});
+
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
